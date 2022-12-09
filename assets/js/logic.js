e16ab8a503af9aa16d5747f08e5cde79fbdea233
JackLabukas <labukasgediminas@gmail.com>
2022-12-09 18:52:29 +0000

    added fixed html and fixed js code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -26,97 +26,142 @@ var questions = [
     answer: ".length",
   },
 ];
-// declare variables
-var submitBtn = document.querySelector("#submit");
-var startBtn = document.querySelector("#start");
-var initialsEle = document.querySelector("#initials");
-var feedBackEle = document.querySelector("#feedback");
-var questionTitle = document.querySelector("#question-title");
-var startScreen = document.querySelector("#start-screen");
-var questionsEle = document.querySelector("#questions");
-var choicesDiv = document.querySelector("#choices");
-var timeEle = document.querySelector("#time");
-var time = 60;
-var score = 0;
-var questionIndex = 0;
-var answerNum = 0;
-timeEle.textContent = time;
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
 
-function startTimer() {
-  var timerInterval = setInterval(function () {
-    time--;
-    timeEle.textContent = time;
+// variables to reference DOM elements....................................
+var timeEl = document.querySelector("#time");
+var startBtn = document.querySelector("#startButton");
+var submitBtn = document.querySelector("#submit-button");
+var titleScreen = document.querySelector("#title-section");
+var quizScreen = document.querySelector("#quiz-section");
+var highScoreScreen = document.querySelector("#highscore-section");
+var highScoreDisplay = document.querySelector("#highscore-display-section");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
 
-    if (time === 0) {
-      clearInterval(timerInterval);
-    }
-  }, 1000);
-}
+var questionsEl = document.querySelector("#question");
+var choicesEl = document.querySelector("#choices");
+
+//create a function to start the game
 function startQuiz() {
-  startTimer();
-  startScreen.classList.add("hide");
-  questionsEle.classList.remove("hide");
+  titleScreen.setAttribute("class", "hide");
+  quizScreen.setAttribute("class", "show");
+  timerId = setInterval(tick, 1000);
+  timeEl.textContent = time;
   getQuestion();
 }
 
-startBtn.addEventListener("click", startQuiz);
+//create a second taken off of a clock
+function tick() {
+  time--;
+  timeEl.textContent = time;
+  if (time <= 0) {
+    quizEnd();
+  }
+}
 
 function getQuestion() {
-  // get current question object from array
-
-  var currentQuestion = questions[questionIndex];
-
-  // update title with current question
-
+  var currentQuestion = questions[currentQuestionIndex];
   var titleEl = document.getElementById("question-title");
-
   titleEl.textContent = currentQuestion.title;
-
-  // clear out any old question choices
-
-  choicesDiv.innerHTML = "";
-
-  // loop over choices
-
+  choicesEl.innerHTML = "";
   currentQuestion.choices.forEach(function (choice, i) {
-    // create new button for each choice
-
     var choiceNode = document.createElement("button");
-
     choiceNode.setAttribute("class", "choice");
-
     choiceNode.setAttribute("value", choice);
-
     choiceNode.textContent = i + 1 + ". " + choice;
-
-    // attach click event listener to each choice
-
     choiceNode.onclick = questionClick;
-
-    // display on the page
-
-    choicesDiv.appendChild(choiceNode);
+    choicesEl.appendChild(choiceNode);
   });
 }
 
+// click on question answer either generate new question or end quiz if final question, and deduct time for answering wrong
 function questionClick() {
   // check if user guessed wrong
-
   if (this.value !== questions[currentQuestionIndex].answer) {
     // penalize time
-
     time -= 15;
 
     if (time < 0) {
       time = 0;
     }
 
-    // display new time on page
+    timeEl.textContent = time;
 
-    timeEle.textContent = time;
-
-    feedBackEle.textContent = "Wrong!";
+    feedbackEl.textContent = "Wrong!";
   } else {
-    feedBackEle.textContent = "Correct!";
+    feedbackEl.textContent = "Correct!";
+  }
+
+  // flash right/wrong feedback on page for half a second
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function () {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+
+  // move to next question
+  currentQuestionIndex++;
+
+  // check if we've run out of questions
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
   }
 }
+
+// end the quiz function
+function quizEnd() {
+  // stop timer
+  clearInterval(timerId);
+
+  // show end screen
+  var highscoreSectionEl = document.querySelector("#highscore-section");
+  highscoreSectionEl.setAttribute("class", "show");
+
+  // show final score
+  var finalScoreEl = document.querySelector("#final-score");
+  finalScoreEl.textContent = time;
+
+  // hide questions section
+  quizScreen.setAttribute("class", "hide");
+}
+
+// function for saving highscore
+function saveHighscore() {
+  // get value of input box
+  var initials = initialsEl.value.trim();
+
+  // make sure value wasn't empty
+  if (initials !== "") {
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    var newScore = {
+      score: time,
+      initials: initials,
+    };
+
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    window.location.href = "highScore.html";
+  }
+}
+
+function checkForEnter(event) {
+  // "13" represents the enter key
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+// user clicks button to submit initials
+submitBtn.onclick = saveHighscore;
+
+// user clicks button to start quiz
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
